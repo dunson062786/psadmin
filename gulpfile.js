e7706d67b2d59cfbfc80efc26ef214b4f1dc34dd
Matt Dunson <dunson.142000@gmail.com>
2018-11-12 18:32:11 -0500

    added bootsrap libraries

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,10 @@ var config = {
 	paths: {
 		html: './src/*.html',
 		js: './src/**/*.js',
+		css: [
+			'node_modules/bootstrap/dist/css/bootstrap.min.css',
+			'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+		],
 		dist: './dist',
 		mainJs: './src/main.js'
 	}
